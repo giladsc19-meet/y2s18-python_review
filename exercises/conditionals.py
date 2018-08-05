9fb95750f829e23ca9566ec102ff5a057506ed08
aboud20-meet <aboud20@meet.mit.edu>
2018-08-05 11:59:07 +0300

    pyt

--- exercises/conditionals.py
+++ exercises/conditionals.py
@@ -1,1 +1,11 @@
-# Write your solution for 1.2 here!
+x=0
+for i in range(101):
+    if i%2==0:
+        x = x+i
+print (x)
+
+x=0
+for i in range(1000):
+    if i%6==2: and i**3%5==3:
+        x=i
+print (x)
